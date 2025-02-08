bc252970b4a71d02d20db12e558d2faa16a85cbd
Karen Khoury <129860242+KarenKhou@users.noreply.github.com>
2025-02-08 22:26:45 +0200

    Add files via upload

---
+++ WEB-INF/src/BonjourServlet.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+
+@WebServlet("/bonjour")
+public class BonjourServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>Bonjour, Nom Prénom</h1>");
+        out.println("</body></html>");
+    }
+}
+
